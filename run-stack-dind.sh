63a5b5fa05988e2ccdd41aea1b91d59d18c216c0
Tolik Litovsky <anatoly.lit@gmail.com>
2019-02-12 15:05:21 +1300

    fixing the run command

--- run-stack-dind.sh
+++ run-stack-dind.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 vagrant up
-echo "docker run -ti --rm -v /vagrant:/vagrant -v /var/run//docker.sock:/var/run/docker.sock  --privileged dockerswarm/dind-master /usr/bin/local/docker stack deploy --compose-file /vagrant/SampleStack/docker-compose-swarm.yml web"| vagrant ssh swarm-master 
+echo "docker stack deploy --compose-file /vagrant/DindStack/dind-stack.yml dind"| vagrant ssh swarm-master 
