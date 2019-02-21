dd28144b2684b6a7d5d4173e43f5aec3834af6e6
Tolik Litovsky <anatoly.lit@gmail.com>
2019-02-21 17:15:18 +1300

    Updating the run file

--- run-stack-dind.sh
+++ run-stack-dind.sh
@@ -4,10 +4,10 @@ vagrant up
 echo "docker stack deploy --compose-file /vagrant/DindStack/dind-stack.yaml dind"| vagrant ssh swarm-master
 echo "waiting for stack to deploy"
 while true;do
-    wget -T 15 -c http://192.168.50.10:8000 && break  &> /dev/null
-    sleep 1
-    echo "."
+    wget -q http://192.168.50.10:8000 && break  
+    sleep 2
+    echo -n "."
 done
-
+echo ""
 echo "http://192.168.50.10:8000 will write counter to a file on shared volume"
 echo "http://192.168.50.10:8001 will read the counter from the file on shared volume"
